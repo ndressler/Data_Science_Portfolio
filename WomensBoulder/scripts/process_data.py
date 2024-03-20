864f29adcb5435f01328aeaafaff13eeb2ded865
Nicole Dressler <nicole.c.dressler@gmail.com>
2024-03-20 21:55:40 +0100

    clean json data

--- WomensBoulder/scripts/process_data.py
+++ WomensBoulder/scripts/process_data.py
@@ -1,7 +1,5 @@
 import json
-import csv
 import os
-import uuid
 
 # define path
 data_dir = os.path.join(os.path.dirname(__file__), '..', 'data')
@@ -12,17 +10,6 @@ with open(raw_data_path, 'r', encoding='utf-8') as f:
 
 # new copy
 data = raw_data[:]
-
-def json_to_csv(output_file):
-
-    headers = list(data[0].keys())
-
-    with open(output_file, 'w', newline='') as f:
-        writer = csv.DictWriter(f, fieldnames=headers)
-        writer.writeheader()
-        writer.writerows(data)
-
-    print("CSV file has been created successfully.")
 
 def add_ids():
     num_entries = len(data)
@@ -58,10 +45,35 @@ def clean_entries():
             entry['city'] = 'VIENNA'
         if 'city' in entry and entry['city'] == 'LA REUNION':
             entry['city'] = 'REUNION'
+
         if 'results' in entry:
             for result in entry['results']:
                 if 'Country' in result:
                     result['Country_Athlete'] = result.pop('Country')
+
+def replace_scores():
+    for competition in data:
+        for result in competition["results"]:
+            # Qualification
+            if "Qualification" in result and result["Qualification"] is not None:
+                if '0T ' in result["Qualification"]:
+                    result["Qualification"] = result["Qualification"].replace('0T ', '0T0 ')
+                if ' 0B' in result["Qualification"]:
+                    result["Qualification"] = result["Qualification"].replace(' 0B', ' 0B0')
+
+            # Semi-final
+            if "Semi-Final" in result and result["Semi-Final"] is not None:
+                if '0T ' in result["Semi-Final"]:
+                    result["Semi-Final"] = result["Semi-Final"].replace('0T ', '0T0 ')
+                if ' 0B' in result["Semi-Final"]:
+                    result["Semi-Final"] = result["Semi-Final"].replace(' 0B', ' 0B0')
+
+            # Final
+            if "Final" in result and result["Final"] is not None:
+                if '0T ' in result["Final"]:
+                    result["Final"] = result["Final"].replace('0T ', '0T0 ')
+                if ' 0B' in result["Final"]:
+                    result["Final"] = result["Final"].replace(' 0B', ' 0B0')
 
 def add_country(city_country):
     for entry in data:
@@ -79,11 +91,73 @@ def country_ath_full_name(country_names):
                     if country_code in country_names:
                         result['Country_Athlete'] = country_names[country_code]
 
+def parse_result_2018up(result):
+    parsed_result = {}
+
+    qualification = result.get("Qualification")
+    if qualification is not None and qualification != "DNS":
+        q_parts = qualification.split(' ')
+        parsed_result["q_top"] = int(q_parts[0][0])
+        parsed_result["q_zone"] = int(q_parts[0][-2])
+        parsed_result["q_top_att"] = int(q_parts[1])
+        parsed_result["q_zone_att"] = int(q_parts[2])
+
+    semi_final = result.get("Semi-Final")
+    if semi_final is not None and semi_final != "DNS":
+        sf_parts = semi_final.split(' ')
+        parsed_result["sf_top"] = int(sf_parts[0][0])
+        parsed_result["sf_zone"] = int(sf_parts[0][-2])
+        parsed_result["sf_top_att"] = int(sf_parts[1])
+        parsed_result["sf_zone_att"] = int(sf_parts[2])
+
+    final = result.get("Final")
+    if final is not None and final != "DNS":
+        f_parts = final.split(' ')
+        parsed_result["f_top"] = int(f_parts[0][0])
+        parsed_result["f_zone"] = int(f_parts[0][-2])
+        parsed_result["f_top_att"] = int(f_parts[1])
+        parsed_result["f_zone_att"] = int(f_parts[2])
+
+    return parsed_result
+
+def parse_result_2017down(result):
+    parsed_result = {}
+
+    qualification = result.get("Qualification")
+    if qualification is not None and qualification != "DNS":
+        q_parts = qualification.split(' ')
+        parsed_result["q_top"] = int(q_parts[0].split('T')[0])
+        parsed_result["q_zone"] = int(q_parts[0].split('T')[1])
+        parsed_result["q_top_att"] = int(q_parts[1].split('B')[0])
+        parsed_result["q_zone_att"] = int(q_parts[1].split('B')[1])
+    else:
+        parsed_result["f_top"] = None
+        parsed_result["f_zone"] = None
+        parsed_result["f_top_att"] = None
+        parsed_result["f_zone_att"] = None
+
+    semi_final = result.get("Semi-Final")
+    if semi_final is not None and semi_final != "DNS":
+        sf_parts = semi_final.split(' ')
+        parsed_result["sf_top"] = int(sf_parts[0].split('T')[0])
+        parsed_result["sf_zone"] = int(sf_parts[0].split('T')[1])
+        parsed_result["sf_top_att"] = int(sf_parts[1].split('B')[0])
+        parsed_result["sf_zone_att"] = int(sf_parts[1].split('B')[1])
+
+    final = result.get("Final")
+    if final is not None and final != "DNS":
+        f_parts = final.split(' ')
+        parsed_result["f_top"] = int(f_parts[0].split('T')[0])
+        parsed_result["f_zone"] = int(f_parts[0].split('T')[1])
+        parsed_result["f_top_att"] = int(f_parts[1].split('B')[0])
+        parsed_result["f_zone_att"] = int(f_parts[1].split('B')[1])
+
+    return parsed_result
 
 
 
 # add country
-print(find_cities())
+#print(find_cities())
 city_country_mapping = {
                         'SALT LAKE CITY': 'UNITED STATES',
                         "TAI'AN": 'CHINA',
@@ -136,11 +210,14 @@ add_country(city_country_mapping)
 # clean data
 clean_entries()
 
+# clean scores
+replace_scores()
+
 # add ids
 add_ids()
 
 # add full country name
-print(find_countries_athlete())
+#print(find_countries_athlete())
 country_at_mapping = {
     'NOR': 'NORWAY',
     'CHI': 'CHILE',
@@ -215,6 +292,15 @@ country_at_mapping = {
 }
 country_ath_full_name(country_at_mapping)
 
+# separate points
+for competition in data:
+    if competition["year"] > 2017:
+        for result in competition["results"]:
+            result.update(parse_result_2018up(result))
+    if competition["year"] <= 2017:
+        for result in competition["results"]:
+            result.update(parse_result_2017down(result))
+
 
 # save data
 json_data_path = os.path.join(data_dir, 'data.json')
@@ -222,5 +308,3 @@ csv_file_path = os.path.join(data_dir, 'data.csv')
 
 with open(json_data_path, 'w', encoding='utf-8') as f:
     json.dump(data, f, indent=4, ensure_ascii=False)
-
-json_to_csv(csv_file_path)
